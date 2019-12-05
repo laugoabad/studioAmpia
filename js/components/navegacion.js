694b0874e73bc1bd1dd84c61abbb8e09358967f3
laugoabad <36644178+laugoabad@users.noreply.github.com>
2019-12-04 21:52:33 -0300

    f

--- js/components/navegacion.js
+++ js/components/navegacion.js
@@ -1,7 +1,7 @@
 Vue.component('navegacion', {
     template: //html
         `<div>
-         <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top mt-3 py-3">
+         <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top py-3">
                 <a class="navbar-brand" href="#"><img src="imgs/5.png" width="30" height="30" alt="favicon">&nbsp;&nbsp;
                     Studio Ampia</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
