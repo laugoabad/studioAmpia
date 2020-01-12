0e1107d599d7b2cd6bbfaf826cabda1136d92d27
laugoabad <36644178+laugoabad@users.noreply.github.com>
2020-01-12 14:59:22 -0300

    r

---
+++ js/components/foot.js
@@ -0,0 +1,22 @@
+Vue.component('foot', {
+    template: //html
+        `<div>
+           <footer class="footer text-center">
+                <h1>
+                   STUDIO AMPIA
+                </h1>
+                <address class="titulo-secundario">
+                    C.so Francia 131 - 1.138, Torino<br>
+                    370 37 08 297<br>
+                    <a href="mailto:ampia.torino@gmail.com">ampia.torino@gmail.com</a><br>
+                </address>
+                
+           </footer>
+        </div>
+        `,
+    data() {
+        return {
+
+        }
+    }
+})
