8b473cd0bf61f0f5664e7eac2c28be8a2b3cbadb
laugoabad <36644178+laugoabad@users.noreply.github.com>
2020-01-12 15:14:31 -0300

    d

--- js/components/foot.js
+++ js/components/foot.js
@@ -2,14 +2,13 @@ Vue.component('foot', {
     template: //html
         `<div>
            <footer class="footer text-center">
-                <h4>
+                <h5>
                    STUDIO AMPIA
                    <address>
                        C.so Francia 131 - 1.138, Torino<br>
-                       370 37 08 297<br>
-                       <a href="mailto:ampia.torino@gmail.com">ampia.torino@gmail.com</a><br>
+                       370 37 08 297 &nbsp;|&nbsp;&nbsp;<a href="mailto:ampia.torino@gmail.com">ampia.torino@gmail.com</a><br>
                     </address>
-                </h4>
+                </h5>
            </footer>
         </div>
         `,
